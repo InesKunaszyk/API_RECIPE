97f1bcf678439d5b5fbff6b6e04b5e62cf193de3
InesKunaszyk <ines.kunaszyk@gmail.com>
2022-11-16 11:25:19 +0100

    recipe serializer

---
+++ app/recipe/serializers.py
@@ -0,0 +1,14 @@
+"""Serializes for recipe APIs"""
+
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """ Seralizers for recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
