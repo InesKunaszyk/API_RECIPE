d8aba4e3642894c0284bb2c43fd39fb6ac5586ef
InesKunaszyk <ines.kunaszyk@gmail.com>
2022-11-21 13:43:29 +0100

    final update with Image logic

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -38,7 +38,7 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializers for view with recipe detail"""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
 
     def _get_or_create_tags(self, tags, recipe):
         """"Handle getting or creating tags as needed"""
